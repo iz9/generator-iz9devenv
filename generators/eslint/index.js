291405a3a3c9d7be88b2bd28fe8e354a31d030e3
iz9 <dev.iz9.l.n@gmail.com>
2021-09-19 09:00:25 +0300

    feat(eslint): overrides for the config dotfiles into generated eslint config

--- generators/eslint/index.js
+++ generators/eslint/index.js
@@ -32,15 +32,23 @@ class EslintGenerator extends require('yeoman-generator') {
   }
 
   async end() {
-    await this.spawnCommand('npx', ['eslint', '--init'])
-
+    this.spawnCommandSync('npx', ['eslint', '--init'])
+    console.log('command')
     const configPath = path.resolve(this.destinationPath(), '.eslintrc.js')
-    let config = require(configPath)
-    config = nextConfigContentString(config, configPath)
+    let config = new EslintConfig()
+    config
+      .readFromFile(configPath)
+      .setIndent(2)
+      .setOverridesForConfigFiles()
+      .addPrettier()
+      .addPrettierOptions()
+      .make()
+    console.log('config made', config.value)
 
     // hack because yoman's this.fs runs any changes in the virtual fs and then only applies it
-    fs.unlinkSync(configPath)
-    fs.writeFileSync(configPath, config)
+    // fs.unlinkSync(configPath
+    console.log('config.toString()', config.toString())
+    fs.writeFileSync(configPath, config.toString())
 
     this.spawnCommandSync('npm', ['set-script', 'prettify', 'prettier --write'])
     this.spawnCommandSync('npm', ['run', 'prettify', './.eslintrc.js'])
@@ -49,30 +57,113 @@ class EslintGenerator extends require('yeoman-generator') {
   }
 }
 
-function mutateEslintConfig(config) {
-  if (!config.plugins) {
-    config.plugins = []
+class EslintConfig {
+  constructor() {
+    this._CONFIG_PLACEHOLDER_ = '__SET_PREVIOUS_CONFIGS_CONTENT_HERE__'
+    this.prevConfig = null
+    this.value = []
+    this.value.push(
+      'const fs = require("fs");',
+      'const path = require("path");',
+      this._CONFIG_PLACEHOLDER_,
+    )
   }
-  config.plugins.push('prettier')
-  config.rules.indent = ['error', 2]
-  config.extends.push('prettier')
-  return config
+
+  readFromFile(pathToConfig) {
+    const prevConfig = require(pathToConfig)
+    if (prevConfig) {
+      // todo: clone prev conf
+      this.prevConfig = prevConfig
+    }
+    return this
+  }
+
+  setIndent(indent = 2) {
+    this.prevConfig.rules.indent = ['error', indent]
+    return this
+  }
+
+  addPrettier() {
+    if (!this.prevConfig.plugins) {
+      this.prevConfig.plugins = []
+    }
+    this.prevConfig.plugins.push('prettier')
+    this.prevConfig.extends.push('prettier')
+
+    return this
+  }
+
+  addPrettierOptions() {
+    this.value.push(
+      '/* ***** Add prettier options **** */;',
+      'const prettierOptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, ".prettierrc"), "utf8"));',
+      'config.rules["prettier/prettier"] = ["error", prettierOptions];',
+    )
+    return this
+  }
+
+  setOverridesForConfigFiles() {
+    if (!this.prevConfig.overrides) {
+      this.prevConfig.overrides = []
+    }
+    this.prevConfig.overrides.push({
+      files: ['./.*.js', './*.config.js'],
+      parser: 'esprima',
+      extends: ['eslint:recommended'],
+      env: { commonjs: true, es2021: true, node: true },
+      parserOptions: { ecmaVersion: 12, sourceType: 'script' },
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    })
+    return this
+  }
+
+  make() {
+    this.value[
+      this.value.indexOf(this._CONFIG_PLACEHOLDER_)
+    ] = `const config = ${JSON.stringify(this.prevConfig)};`
+    this.value.push('module.exports=config;')
+    return this
+  }
+
+  toString() {
+    return this.value.join('\n')
+  }
 }
 
-function nextConfigContentString(config) {
-  const content = []
-  config = mutateEslintConfig(config)
-  content.push('const fs = require("fs");')
-  content.push('const path = require("path");')
-  content.push(`const prettierOptions = JSON.parse(
-    fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
-  );`)
-  content.push(`const config = ${JSON.stringify(config)};`)
-  content.push(
-    'config.rules["prettier/prettier"] = ["error", prettierOptions];',
-  )
-  content.push('module.exports = config')
-  return content.join('\n')
-}
+// function buildNextEslintConfigToString(config){
+//   if (!config.plugins) {
+//     config.plugins = []
+//   }
+//   config.plugins.push('prettier')
+//   config.rules.indent = ['error', 2]
+//   config.extends.push('prettier')
+//
+//   config.
+// }
+//
+//
+//
+// function mutateEslintConfig(config) {
+//
+//   return config
+// }
+//
+// function nextConfigContentString(config) {
+//   const content = []
+//   config = mutateEslintConfig(config)
+//   content.push('const fs = require("fs");')
+//   content.push('const path = require("path");')
+//   content.push(`const prettierOptions = JSON.parse(
+//     fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+//   );`)
+//   content.push(`const config = ${JSON.stringify(config)};`)
+//   content.push(
+//     'config.rules["prettier/prettier"] = ["error", prettierOptions];',
+//   )
+//   content.push('module.exports = config')
+//   return content.join('\n')
+// }
 
 module.exports = EslintGenerator
